43078ec0edaa008d187d554bbc19406eeb2c2a29
josiah-horst1 <148979023+josiah-horst1@users.noreply.github.com>
2023-12-02 18:44:23 -0500

    Added comments to cell.py

--- cell.py
+++ cell.py
@@ -5,11 +5,23 @@ class Cell:
         self.col = col
         self.screen = screen
 
+    '''
+    Setter for this cell’s value
+    '''
     def set_cell_value(self, value):
         pass
 
+    '''
+    Setter for this cell’s sketched value
+    '''
     def set_sketched_value(self, value):
         pass
 
+    '''
+    Draws this cell, along with the value inside it.
+    If this cell has a nonzero value, that value is displayed.
+    Otherwise, no value is displayed in the cell.
+    The cell is outlined red if it is currently selected.
+    '''
     def draw(self):
         pass
