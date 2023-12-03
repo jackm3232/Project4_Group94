ba34101e9148c9bbf610879d7cb006fff53bf041
josiah-horst1 <148979023+josiah-horst1@users.noreply.github.com>
2023-12-03 16:03:05 -0500

    Added lines to the screen

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame, sys
-from sudoku_generator import *
+#from sudoku_generator import *
 
 pygame.init()
-pygame.display_set_caption('Sudoku')
+pygame.display.set_caption('Sudoku')
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((675, 675))
+screen.fill((255, 255, 255))
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        pygame.draw.line(screen, (0, 0, 0), (225, 0), (225, 600))
+        pygame.draw.line(screen, (0, 0, 0), (450, 0), (450, 600))
+        pygame.draw.line(screen, (0, 0, 0), (0, 225), (675, 225))
+        pygame.draw.line(screen, (0, 0, 0), (0, 450), (675, 450))
+        pygame.display.update()
+    if event.type == pygame.QUIT:
+        break
+      
