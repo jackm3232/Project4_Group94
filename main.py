45650a2b1b7f714b5cdae999b468b128d31d133d
fdeboom <faithdeboom@gmail.com>
2023-12-04 15:21:22 -0500

    created exit ability and improved readability of code

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame, sys
 #from sudoku_generator import *
 
-pygame.init()
-pygame.display.set_caption('Sudoku')
 
+pygame.init()
 screen = pygame.display.set_mode((675, 675))
+pygame.display.set_caption('Sudoku')
 screen.fill((255, 255, 255))
 pygame.display.update()
 
 while True:
-    for event in pygame.event.get():
+    for event in pygame.event.get(): # event loop
         pygame.draw.line(screen, (0, 0, 0), (225, 0), (225, 600), width=10)
         pygame.draw.line(screen, (0, 0, 0), (450, 0), (450, 600), width=10)
         pygame.draw.line(screen, (0, 0, 0), (0, 200), (675, 200), width=10)
@@ -19,6 +19,7 @@ while True:
         pygame.draw.line(screen, (0, 0, 0), (4, 0), (4, 600), width=10)
         pygame.draw.line(screen, (0, 0, 0), (669, 0), (669, 600), width=10)
         pygame.display.update()
-    if event.type == pygame.QUIT:
-        break
-        
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
