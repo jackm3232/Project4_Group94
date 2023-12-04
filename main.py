230b732b6b09937f324f91adddd6740a5ee789b1
josiah-horst1 <148979023+josiah-horst1@users.noreply.github.com>
2023-12-04 13:08:44 -0500

    Updated the screen to have a grid

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ pygame.display.update()
 
 while True:
     for event in pygame.event.get():
-        pygame.draw.line(screen, (0, 0, 0), (225, 0), (225, 600))
-        pygame.draw.line(screen, (0, 0, 0), (450, 0), (450, 600))
-        pygame.draw.line(screen, (0, 0, 0), (0, 225), (675, 225))
-        pygame.draw.line(screen, (0, 0, 0), (0, 450), (675, 450))
+        pygame.draw.line(screen, (0, 0, 0), (225, 0), (225, 600), width=10)
+        pygame.draw.line(screen, (0, 0, 0), (450, 0), (450, 600), width=10)
+        pygame.draw.line(screen, (0, 0, 0), (0, 200), (675, 200), width=10)
+        pygame.draw.line(screen, (0, 0, 0), (0, 400), (675, 400), width=10)
+        pygame.draw.line(screen, (0, 0, 0), (0, 600), (675, 600), width=10)
+        pygame.draw.line(screen, (0, 0, 0), (0, 4), (675, 4), width=10)
+        pygame.draw.line(screen, (0, 0, 0), (4, 0), (4, 600), width=10)
+        pygame.draw.line(screen, (0, 0, 0), (669, 0), (669, 600), width=10)
         pygame.display.update()
     if event.type == pygame.QUIT:
         break
-      
+        
