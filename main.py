f347d239bab4dad36878fc5f24bde9f48639eceb
fdeboom <faithdeboom@gmail.com>
2023-12-05 00:34:09 -0500

    created and tested button functionality in main game play window

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ screen.fill(BG)
 pygame.display.update()
 num_font = pygame.font.Font(None, NUM_FONT)
 game_over_font = pygame.font.Font(None, OVER_FONT)
+button_font = pygame.font.Font(None, BUTTON_FONT)
 
 size = 0
 remove = 0
-# intitalize game state
-#board = generate_sudoku(size, remove)  # create board
 
+
+# initialize game state
+# board = generate_sudoku(size, remove)  # create board
 
 
 def draw_big_grid():  # large grid
@@ -29,16 +31,19 @@ def draw_big_grid():  # large grid
 
 
 def draw_small_grid():  # inner, smaller grid
-    # draws horizontal lines
-    for i in range(9):
+    # draws vertical lines
+    for i in range(10):
         pygame.draw.line(screen, BLACK, (SMALL_SQUARE * i, START), (SMALL_SQUARE * i, END),
                          SMALL_LINE_WIDTH)
-    # draws vertical lines
+    # draws horizontal lines
     for i in range(9):
         pygame.draw.line(screen, BLACK, (START, SMALL_SQUARE * i), (END, SMALL_SQUARE * i),
                          SMALL_LINE_WIDTH)
+
+
 def highlight_cell(row, col):
-    pygame.draw.line(screen, RED, (SMALL_SQUARE*col, SMALL_SQUARE*row), (SMALL_SQUARE*col, SMALL_SQUARE*row + SMALL_SQUARE),
+    pygame.draw.line(screen, RED, (SMALL_SQUARE * col, SMALL_SQUARE * row),
+                     (SMALL_SQUARE * col, SMALL_SQUARE * row + SMALL_SQUARE),
                      SMALL_LINE_WIDTH)
     pygame.draw.line(screen, RED, (SMALL_SQUARE * col, SMALL_SQUARE * row),
                      (SMALL_SQUARE * col + SMALL_SQUARE, SMALL_SQUARE * row),
@@ -49,11 +54,38 @@ def highlight_cell(row, col):
     pygame.draw.line(screen, RED, (SMALL_SQUARE * col, SMALL_SQUARE * row + SMALL_SQUARE),
                      (SMALL_SQUARE * col + SMALL_SQUARE, SMALL_SQUARE * row + SMALL_SQUARE),
                      SMALL_LINE_WIDTH)
+
+
 def draw_numbers():
     num_one_surf = num_font.render("1", 0, BLACK)
-    num_one_rect = num_one_surf.get_rect(center = (340,340))
+    num_one_rect = num_one_surf.get_rect(center=(340, 340))
 
     screen.blit(num_one_surf, num_one_rect)
+
+
+def buttons():
+    reset_text = button_font.render("RESET", 0, WHITE)
+    reset_surface = pygame.Surface((reset_text.get_size()[0] + 20, reset_text.get_size()[1] + 20))
+    reset_surface.fill(ORANGE)
+    reset_surface.blit(reset_text, (10, 10))
+    reset_rectangle = reset_surface.get_rect(
+        center=(WIDTH//4, 715))
+    screen.blit(reset_surface, reset_rectangle)
+    restart_text = button_font.render("RESTART", 0, WHITE)
+    restart_surface = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
+    restart_surface.fill(ORANGE)
+    restart_surface.blit(restart_text, (10, 10))
+    restart_rectangle = restart_surface.get_rect(
+        center=(WIDTH//2, 715))
+    screen.blit(restart_surface, restart_rectangle)
+    exit_text = button_font.render("EXIT", 0, WHITE)
+    exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
+    exit_surface.fill(ORANGE)
+    exit_surface.blit(exit_text, (10, 10))
+    exit_rectangle = exit_surface.get_rect(
+        center=(3*WIDTH//4-10, 715))
+    screen.blit(exit_surface, exit_rectangle)
+
 
 def draw_game_over(win):
     screen.fill(BG)
@@ -62,8 +94,9 @@ def draw_game_over(win):
     else:
         end_text = "Game Won!"
     end_surf = game_over_font.render(end_text, 0, BLACK)
-    end_rect = end_surf.get_rect(center = (WIDTH // 2, HEIGHT // 3))
-    screen.blit(end_surf, end_text)
+    end_rect = end_surf.get_rect(center=(WIDTH // 2, HEIGHT // 3))
+    screen.blit(end_surf, end_rect)
+
 
 while True:
     game_over = False
@@ -73,6 +106,7 @@ while True:
         draw_small_grid()
         pygame.display.update()
         draw_numbers()
+        buttons()
         # initialize board with basic fill in
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -81,7 +115,16 @@ while True:
             x, y = event.pos
             row = y // SMALL_SQUARE
             col = x // SMALL_SQUARE
-            highlight_cell(row, col)
+            if y <= 675:
+                highlight_cell(row, col)
+            else:
+                if y <= 739 and y >= 691:
+                    if x >= 112 and x <= 224:
+                        print('reset')  # reset board
+                    elif x >= 263 and x <= 411:
+                        print('restart')  # return to home screen
+                    if x >= 460 and x <= 548:
+                        print('exit')  # end program
         if game_over:
             pygame.display.update()
             pygame.time.delay(100)
