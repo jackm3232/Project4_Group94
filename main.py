d2476e5bdc1f246a9801c6118001c3c74167a778
fdeboom <faithdeboom@gmail.com>
2023-12-04 20:49:46 -0500

    deleted commented earlier code

--- main.py
+++ main.py
@@ -28,14 +28,6 @@ while True:
     for event in pygame.event.get(): # event loop
         draw_big_grid()
         draw_small_grid()
-        """ pygame.draw.line(screen, WHITE, (225, 0), (225, 600), width=10)
-        pygame.draw.line(screen, WHITE, (450, 0), (450, 600), width=10)
-        pygame.draw.line(screen, WHITE, (0, 200), (675, 200), width=10)
-        pygame.draw.line(screen, WHITE, (0, 400), (675, 400), width=10)
-        pygame.draw.line(screen, WHITE, (0, 600), (675, 600), width=10)
-        pygame.draw.line(screen, WHITE, (0, 4), (675, 4), width=10)
-        pygame.draw.line(screen, WHITE, (4, 0), (4, 600), width=10)
-        pygame.draw.line(screen, WHITE, (669, 0), (669, 600), width=10)"""
         pygame.display.update()
         if event.type == pygame.QUIT:
             pygame.quit()
