985db57c83b4f509eef2d8156d49f540a30cc383
fdeboom <faithdeboom@gmail.com>
2023-12-05 12:44:12 -0500

    created blank board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame, sys
+
+from Project4_Group94.sudoku_generator import generate_sudoku
 # from sudoku_generator import *
 from constants import *
 
@@ -56,9 +58,9 @@ def highlight_cell(row, col):
                      SMALL_LINE_WIDTH)
 
 
-def draw_numbers():
+def draw_numbers(row, col):
     num_one_surf = num_font.render("1", 0, BLACK)
-    num_one_rect = num_one_surf.get_rect(center=(340, 340))
+    num_one_rect = num_one_surf.get_rect(center=(row * 40 + SMALL_SQUARE, col * 40 + SMALL_SQUARE))
 
     screen.blit(num_one_surf, num_one_rect)
 
@@ -69,21 +71,21 @@ def buttons():
     reset_surface.fill(ORANGE)
     reset_surface.blit(reset_text, (10, 10))
     reset_rectangle = reset_surface.get_rect(
-        center=(WIDTH//4, 715))
+        center=(WIDTH // 4, 715))
     screen.blit(reset_surface, reset_rectangle)
     restart_text = button_font.render("RESTART", 0, WHITE)
     restart_surface = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
     restart_surface.fill(ORANGE)
     restart_surface.blit(restart_text, (10, 10))
     restart_rectangle = restart_surface.get_rect(
-        center=(WIDTH//2, 715))
+        center=(WIDTH // 2, 715))
     screen.blit(restart_surface, restart_rectangle)
     exit_text = button_font.render("EXIT", 0, WHITE)
     exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
     exit_surface.fill(ORANGE)
     exit_surface.blit(exit_text, (10, 10))
     exit_rectangle = exit_surface.get_rect(
-        center=(3*WIDTH//4-10, 715))
+        center=(3 * WIDTH // 4 - 10, 715))
     screen.blit(exit_surface, exit_rectangle)
 
 
@@ -98,32 +100,46 @@ def draw_game_over(win):
     screen.blit(end_surf, end_rect)
 
 
+def draw_og_board(b):
+    offset = 20
+    for i in range(len(b)):
+        for j in range(len(b[i])):
+            num = b[i][j]
+            if num != 0:
+                num_text = num_font.render(str(num), 0, BLACK)
+                screen.blit(num_text, ((i * SMALL_SQUARE) + offset + 5, (j * SMALL_SQUARE) + offset - 5))
+
+
+# initialize board with all empty cells
+diff = 30
+board = generate_sudoku(9, diff)
+draw_big_grid()
+draw_small_grid()
+draw_og_board(board)
+buttons()
+pygame.display.update()
 while True:
     game_over = False
     for event in pygame.event.get():  # event loop
-        # initialize board with all empty cells
         draw_big_grid()
         draw_small_grid()
         pygame.display.update()
-        draw_numbers()
-        buttons()
-        # initialize board with basic fill in
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
             x, y = event.pos
-            row = y // SMALL_SQUARE
-            col = x // SMALL_SQUARE
+            r = y // SMALL_SQUARE
+            c = x // SMALL_SQUARE
             if y <= 675:
-                highlight_cell(row, col)
+                highlight_cell(r, c)
             else:
-                if y <= 739 and y >= 691:
-                    if x >= 112 and x <= 224:
+                if 739 >= y >= 691:
+                    if 112 <= x <= 224:
                         print('reset')  # reset board
-                    elif x >= 263 and x <= 411:
+                    elif 263 <= x <= 411:
                         print('restart')  # return to home screen
-                    if x >= 460 and x <= 548:
+                    if 460 <= x <= 548:
                         print('exit')  # end program
         if game_over:
             pygame.display.update()
